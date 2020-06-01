36881949e14b8a77a82859240c26e7a76d5ef4bb
gstoica27 <gstoica27@gmail.com>
2020-05-31 17:29:37 -0700

    added semeval to cwd

--- semeval/train.py
+++ semeval/train.py
@@ -37,7 +37,7 @@ def add_kg_model_params(cfg_dict, cwd):
 cwd = os.getcwd()
 on_server = 'Desktop' not in cwd
 dataset = 'semeval'
-config_path = os.path.join(cwd, 'configs', dataset, f'{"nell" if on_server else "local"}_config.yaml')
+config_path = os.path.join(cwd, 'semeval', 'configs', dataset, f'{"nell" if on_server else "local"}_config.yaml')
 # config_path = os.path.join(cwd, 'configs', 'nell_config.yaml')
 with open(config_path, 'r') as file:
     cfg_dict = yaml.load(file)
