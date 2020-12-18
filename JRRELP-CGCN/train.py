c8e7531f08b1d294da81e4ca7bf16d3484a4e480
gstoica27 <gstoica27@gmail.com>
2020-12-17 16:28:43 -0800

    Altered CGCN config to be base_config

--- JRRELP-CGCN/train.py
+++ JRRELP-CGCN/train.py
@@ -60,9 +60,7 @@ def add_kg_model_params(cfg_dict, cwd):
     return params
 
 cwd = os.getcwd()
-on_server = 'Desktop' not in cwd
-config_path = os.path.join(cwd, 'configs', f'{"nell" if on_server else "local"}_config.yaml')
-# config_path = os.path.join(cwd, 'configs', 'nell_config.yaml')
+config_path = os.path.join(cwd, 'configs', 'base_config.yaml')
 with open(config_path, 'r') as file:
     cfg_dict = yaml.load(file)
 
